07fcac6fc3b8a853a3264b7b1dea92335f04c33c
Sandeep-Kumar-1994 <sandeepvoices@gmail.com>
2018-07-19 01:54:39 +0530

    Studyspace

--- app1/views.py
+++ app1/views.py
@@ -2,13 +2,44 @@ from django.shortcuts import render
 from app1.models import Studyhall,Expense,Enquiry,Course,Student
 
 def funs(request):
+	if request.method=="POST":
+		data=request.POST
+		if data.get("enquiry"):
+			course_inst = Course.objects.get(pk=data.get("enq_course"))
+			stdudent_inst = Student.objects.get(pk=data.get("enq_student"))
+			enq=Enquiry(
+				name=data.get("enq_name"),
+				student=stdudent_inst,
+				course=course_inst
+				)
+			enq.save()
+		elif data.get("expense"):
+			studyhall_inst=Studyhall.objects.get(pk=data.get("exp_studyhall"))
+			exp=Expense(
+				studyhall=studyhall_inst,
+				date=data.get("exp_date"),
+				name=data.get("exp_name"),
+				desc=data.get("exp_desc"),
+				value=data.get("exp_value"),
+				)
+			exp.save()
+		else:
+			hall=Studyhall(name=data.get("hall_name"),
+				area=data.get("hall_area"))
+			hall.save()
+
 	studyhalls = Studyhall.objects.all()
 	expenses=Expense.objects.all()
 	enquiries=Enquiry.objects.all()
+	courses=Course.objects.all()
+	students=Student.objects.all()
 	return render(request,"app1/index.html",{
 		"studyhalls":studyhalls,
 		"expenses":expenses,
-		"enquiries":enquiries})
+		"enquiries":enquiries,
+		"students":students,
+		"courses":courses		
+		})
 	
 	# return render(request,"app1/index.html")
 # Create your views here.
@@ -16,8 +47,5 @@ def funs(request):
 def view_studyhalls(request):
 	studyhalls=Studyhall.objects.all()
 	return render(request,"app1/index.html",{"studyhalls":studyhalls})
-	expenses=Expense.objects.all()
-	return render(request,"app1/index.html",{"expenses":expenses})
-	enquiries=Enquiry.objects.all()
-	return render(request,"app1/index.html",{"enquiries":enquiries})
+	
 
